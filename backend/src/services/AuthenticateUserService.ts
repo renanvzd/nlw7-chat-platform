fa43d94ee84fcab98dfc9db3dbb1b85d3ebfcfb4
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-19 18:45:21 -0300

    Token jwt

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 /**
  * Receber code (string)
  * Recuperar o access_token no github (que o próprio github disponibiliza para acessar infos do usuario)
+ * Recuperar infos do user no github
  * Verificar se o usuário existe no DB
  
         * SIM, existe = Gera um token
@@ -10,11 +11,22 @@
 
 import axios from "axios";
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -22,8 +34,15 @@ class AuthenticateUserService {
       },
       headers: {
         "Accept": "application/json"
+      },
+    });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
       }
     })
+
     return response.data
   }
 }
