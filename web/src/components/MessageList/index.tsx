8b327d68be642efa1b54d95efda2d4c6a9f68849
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-20 19:15:40 -0300

    Exibir msgs do backend no frontend

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,14 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api'
-
 import styles from './styles.module.scss';
-
 import logoImg from '../../assets/logo.svg';
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
   useEffect(() => {
-    api.get('messages/last3').then(response => {
-      console.log(response.data);
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
     })
   }, [])
 
@@ -17,16 +26,19 @@ export function MessageList() {
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar o evento, com certeza vai
-            ser o melhor</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/renanvzd.png" alt="Renan Veronez" />
-            </div>
-            <span>Renan Veronez</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   )
