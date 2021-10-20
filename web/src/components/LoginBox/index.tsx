0ed8f9177d564250f3b73d93c29e434d3abb68e6
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-20 19:46:42 -0300

    Login github

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -3,10 +3,12 @@ import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss'
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=40f133add502b3d603c8`;
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
