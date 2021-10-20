b31c39e7c7817b80d03bc23fd84f38ac4649717b
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-20 20:17:46 -0300

    Pegar dados do usuario

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,9 +1,41 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss'
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=40f133add502b3d603c8`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+    const { token, user } = response.data;
+    localStorage.setItem('@dowhile:token', token)
+    console.log(user)
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+      window.history.pushState({}, '', urlWithoutCode);
+      signIn(githubCode)
+    }
+  })
 
   return (
     <div className={styles.loginBoxWrapper}>
