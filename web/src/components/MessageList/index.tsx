2f323057f895b27cf517439ad2d0d65da0b3c1c8
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-20 16:53:03 -0300

    Message list

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,24 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar o evento, com certeza vai
+            ser o melhor</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/renanvzd.png" alt="Renan Veronez" />
+            </div>
+            <span>Renan Veronez</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
